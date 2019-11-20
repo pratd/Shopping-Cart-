761cc83403c11efbfd22985281c60d83ff65eb09
prattya datta <pratt@LAPTOP-500LE5P5.(none)>
2019-11-20 11:46:16 +0100

    Start of options

--- Js/Productdatabase.js
+++ Js/Productdatabase.js
@@ -6,10 +6,11 @@ let ProductDatabase = [
         logo:"assets/item3.jpg",
         quantity: 50,
         options: {
-            More: [{
-                colors: ['red','white', 'blue', 'black', 'gray'],
-                sizes:['S','M','L','XL','XXL']
-            }]
+            colors: ['red','white', 'blue', 'black', 'gray'],
+            sizes:['S','M','L','XL','XXL'],
+            Price:[[100,100,100,100,100],[120,120,120,120,120],[110,110,110,110,110],
+            [130,130,130,130,130],[100,100,100,100,100]],
+            stock:[[10,20,20,30,4],[15,34,7,9,10],[17,13,4,5,10],[2,15,17,19,9],[10,20,30,40,10]]
         }
     },
     {
@@ -19,9 +20,9 @@ let ProductDatabase = [
         logo: 'assets/item1.jpg',
         quantity: 5,
         options:{
-            More:[{
-                colors:['red','white', 'blue', 'black', 'gray']
-            }]
+            colors:['red','white', 'blue', 'black', 'gray'],
+            Price:[3000000,3005000, 3010000,3015000,3010000,3005000],
+            Stock:[3,2,0,1,4]
         }
     },
     {
@@ -31,9 +32,9 @@ let ProductDatabase = [
         logo: 'assets/item5.png',
         quantity: 20,
         options:{
-            More:[{
-                type:['red','white']
-            }]
+            colors:['red','white'],
+            Price:[200,300],
+            Stock:[10,12]
         }
     },
     {
@@ -43,10 +44,11 @@ let ProductDatabase = [
         logo: 'assets/item4.jpg',
         quantity:2,
         options:{
-            More:[{
-                type:['matte black','gray'],
-                additions:['extra armor', 'extra shells']
-            }]
+            color:['matte black','gray'],
+            additions:['extra armor', 'extra shells'],
+            Price:[[6200000,7000000],[5000000,5200000]],
+            Stock:[[2,3],[1,1]]
+
         }
     },
     {
@@ -55,9 +57,9 @@ let ProductDatabase = [
         type:'House deco',
         logo: 'assets/item2.jpeg',
         quantity:50,
-        More:[{
-            type:['with flower', 'without flower']
-        }]
+        typeofPlant:['with flower', 'without flower'],
+        Price:[10,5],
+        Stock:[30,50],
     },
     {
         name:'Macbook Pro',
@@ -66,10 +68,10 @@ let ProductDatabase = [
         logo: 'assets/item6.jpg',
         quantity:20,
         options:{
-            More:[{
-                RAM:['6GB','8GB','16GB'],
-                color:['Matte black','matte white','silver gray']
-            }]
+            RAM:['6GB','8GB','16GB'],
+            colors:['Matte black','matte white','silver gray'],
+            Price:[[3500, 3500, 3500],[3700,3700,3700],[4000,400,4000]],
+            Stock:[[10,10,10],[15,20,34],[3,5,10]]
         }
     }
 ]
--- Js/myjavascript_vanilla.js
+++ Js/myjavascript_vanilla.js
@@ -3,7 +3,6 @@ let Itemselement = document.querySelector('#items');
 function renderProducts(){
     Cardarray.forEach(element => {
 
-
         let newNode = document.createElement('div');
         newNode.classList.add('card','col-sm-4');
 
@@ -15,7 +14,7 @@ function renderProducts(){
         newNodeTitle.textContent = element['name'];
         //adding image
         let newNodeImage = document.createElement('img');
-        newNodeImage.classList.add('card-image'); 
+        newNodeImage.classList.add('img-fluid','card-image'); 
         /*newNodeImage.style.width = '200px';*/
         newNodeImage.style.height = '250px';
         newNodeImage.src = element['logo'];
@@ -23,31 +22,55 @@ function renderProducts(){
         let newNodePrice = document.createElement('p');
         newNodePrice.classList.add('card-text');
         newNodePrice.textContent = element['priceofProduct'] + '€';
-        //buttons
-        let newNodebuttonleft = document.createElement('button');
-        newNodebuttonleft.classList.add('btn', 'btn-primary', 'dropbtn');
-        newNodebuttonleft.textContent = '-';
-        newNodebuttonleft.setAttribute('marcador', element['type']);
-        //newNodebutton.addEventListener('click', AddtoCart);
 
         //creating the blank space to increment the numbers 
-        let newNodeNumberItems
+        let newNodeNumberItems= document.createElement('input');
+        newNodeNumberItems.classList.add('form-control','boxed', 'inlineElement');
+        newNodeNumberItems.placeholder='Add';
+        newNodeNumberItems.id = element['typeofProduct'];
+        newNodeNumberItems.value=0;
+        //buttons
+        let newNodebuttonleft = document.createElement('button');
+        newNodebuttonleft.classList.add('btn', 'btn-primary', 'dropbtn','inlineElement');
+        newNodebuttonleft.textContent = '-';
+        newNodebuttonleft.setAttribute('marketbuy', element['typeofProduct']);
+        newNodebuttonleft.addEventListener('click', UpdateValueDecrement);
+
         let newNodebuttonright = document.createElement('button');
-        newNodebuttonright.classList.add('btn', 'btn-primary', 'dropbtn');
+        newNodebuttonright.classList.add('btn', 'btn-primary', 'dropbtn', 'inlineElement');
         newNodebuttonright.textContent = '+';
-        newNodebuttonright.setAttribute('marcador', element['type']);
-        //newNodebutton.addEventListener('click', AddtoCart);
+        newNodebuttonright.setAttribute('marketbuy', element['typeofProduct']);
+        newNodebuttonright.addEventListener('click', UpdateValueIncrement);
+
+        //select options 
+        let newNodeSelectbody = document.createElement('div');
+        newNodeSelectbody.classList.add('options','col-sm-2');
+
+        let newNodeSelect = document.createElement('select');
+        
+
 
         newNodeCardbody.appendChild(newNodeTitle);
         newNodeCardbody.appendChild(newNodeImage);
         newNodeCardbody.appendChild(newNodePrice);
         newNodeCardbody.appendChild(newNodebuttonleft);
+        newNodeCardbody.appendChild(newNodeNumberItems);
         newNodeCardbody.appendChild(newNodebuttonright);
         newNode.appendChild(newNodeCardbody);
         Itemselement.appendChild(newNode);
 
+        
         //console.log(element)
     });
 }
-
 renderProducts();
+
+function UpdateValueIncrement(){
+   var elem = this.getAttribute('marketbuy');
+    document.getElementById(elem).value = parseInt (document.getElementById(elem).value) +1;
+}
+
+function UpdateValueDecrement(){
+    var elem = this.getAttribute('marketbuy');
+     document.getElementById(elem).value = Math.max(parseInt (document.getElementById(elem).value) -1,0);
+ }
